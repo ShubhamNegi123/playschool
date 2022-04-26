f0d788b53a1de4def86b0999cc67b7f52ac687e5
Shubham Negi <95840309+ShubhamNegi123@users.noreply.github.com>
2022-04-26 18:16:35 +0530

    Add files via upload

--- contact.php
+++ contact.php
@@ -5,12 +5,11 @@ $name = $_REQUEST['name'] ;
 $phone = $_REQUEST['phone'] ;
 $email = $_REQUEST['email'] ;
 $message = $_REQUEST['message'] ;
-$services = $_REQUEST['services'];
 $subject = "Message from: $name";
 $headers = "wallcommunication@gmail.com";
-$body = "From: $name \n\n Email: $email \n\n Phone: $phone \n\n  
-message: $message \n\n services : $services";
-$sent = mail($to, $subject, $body);
+$body = "Inquery from Schooldesign.in \n\n name : $name \n\n Email: $email \n\n Phone: $phone \n\n  
+message: $message \n\n ";
+$sent = mail($to, $subject, $body) ;
 if($sent)
 {echo "<script language=javascript>window.location = 'index.html';</script>";}
 else
